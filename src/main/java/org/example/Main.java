449c8e6cb4e9dcb3be26f2ef8f720e4c03088b12
Shurikosa <shurikosaula@gmail.com>
2023-05-28 21:41:36 +0300

    convertUsersListToJsonFile added. and others.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
 
 import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
 
-    private static final String PATH = "/home/oleksandr/IdeaProjects/Module10IO/src/main/resources/file.txt";
+    private static final String PATH_TO_NUMBERS = "src/main/resources/numbers.txt";
+    private static final String PATH_TO_USERS = "src/main/resources/users.txt";
+    private static final String PATH_TO_JSON_FILE = "src/main/resources/users.json";
     public static void main(String[] args) {
 
-    chekCorrectNumberFormat(PATH);
+    chekCorrectNumberFormat(PATH_TO_NUMBERS);
+    convertUsersListToJsonFile(createUsersList(readUsersFromFile(PATH_TO_USERS)));
 
     }
 
     public static void chekCorrectNumberFormat(String path){
-        File file = new File(PATH);
+        File file = new File(path);
         try (FileReader fileReader = new FileReader(file)){
 
             char[]buf = new char[(int) file.length()];
@@ -26,11 +34,49 @@ public class Main {
                     System.out.println(number);
                 }
             }
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String[] readUsersFromFile (String path){
+        File file = new File(path);
+        String[] users;
+        try(FileReader fileReader = new FileReader(file)){
+            char[] buf = new char[(int) file.length()];
+            fileReader.read(buf);
+            String fileContent = new String(buf);
+            users = fileContent.split("\n");
+            return users;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<User> createUsersList (String[] usersToRedag){
+        List<User> usersList = new ArrayList<>();
+        for ( String fields : usersToRedag) {
+            String[] usersFields = fields.split(" ");
+            User user = new User(usersFields[0],usersFields[1]);
+            usersList.add(user);
+        }
+        return usersList;
+    }
+
+    public static void convertUsersListToJsonFile(List<User> usersList){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try(FileWriter writer = new FileWriter(PATH_TO_JSON_FILE)){
+            gson.toJson(usersList, writer);
+            System.out.println("JSON-file created");
+        }catch (IOException e){
+            System.err.println("Error in writing to the JSON-file ");
+        }
+
+
     }
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class User {
+    private String name;
+    private String age;
+     public User(String name, String age){
+         this.name = name;
+         this.age = age;
+     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+
+}
